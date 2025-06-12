0fa1f4be43677ab9f3fa2486da35008781ce4af0
Day-Ham <daehankimm17@gmail.com>
2025-06-12 20:56:36 +0800

    Replaced It with a Like Button

--- src/components/LikeButton/LikeButton.tsx
+++ src/components/LikeButton/LikeButton.tsx
@@ -2,26 +2,28 @@ import React, { useState } from "react";
 import styles from "./LikedButton.module.css";
 import { AiFillHeart } from "react-icons/ai";
 import { AiOutlineHeart } from "react-icons/ai";
+import { AiFillLike } from "react-icons/ai";
+import { AiOutlineLike } from "react-icons/ai";
 const LikeButton = () => {
   const [toggle, SetToggle] = useState(true);
 
   if (toggle)
     return (
-      <AiFillHeart
+      <AiFillLike
         className={[styles.on].join(" ")}
         onClick={() => {
           SetToggle(false);
         }}
-      ></AiFillHeart>
+      ></AiFillLike>
     );
   else
     return (
-      <AiOutlineHeart
+      <AiOutlineLike
         className={[styles.off].join(" ")}
         onClick={() => {
           SetToggle(true);
         }}
-      ></AiOutlineHeart>
+      ></AiOutlineLike>
     );
 };
 
